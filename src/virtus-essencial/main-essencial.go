57ced8950698944a1b0d7c641524ed26c570134d
Masaru Ohashi Júnior <masaru@vindixit.com>
2021-04-08 17:05:31 -0300

    CRUD de Planos em Entidades - campos hidden

--- src/virtus-essencial/main-essencial.go
+++ src/virtus-essencial/main-essencial.go
@@ -79,7 +79,7 @@ func main() {
 	sConfig := ReadConfig(SERVER)
 	sec.Store = sessions.NewCookieStore([]byte(sConfig.EncryptionKey))
 	hd.Db = dbConn()
-	mdl.Ambiente = " [" + sConfig.Ambiente + " 1.2.1" + "]"
+	mdl.Ambiente = " [" + sConfig.Ambiente + " 1.2.2" + "]"
 	mdl.AppName += mdl.Ambiente
 	// injetando a variável Authenticated
 	if true {
