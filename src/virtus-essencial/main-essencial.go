21cb87dcb8ace86bc455338887d4d22c246eb02b
Masaru Ohashi Jr <mohashi@avaya.com>
2021-10-20 17:15:55 -0300

    remoção status inicial

--- src/virtus-essencial/main-essencial.go
+++ src/virtus-essencial/main-essencial.go
@@ -82,7 +82,7 @@ func main() {
 	sConfig := ReadConfig(SERVER)
 	sec.Store = sessions.NewCookieStore([]byte(sConfig.EncryptionKey))
 	hd.Db = dbConn()
-	mdl.Ambiente = " [" + sConfig.Ambiente + " 1.3.5.20211016" + "]"
+	mdl.Ambiente = " [" + sConfig.Ambiente + " 1.3.5.20211020" + "]"
 	mdl.AppName += mdl.Ambiente
 	// injetando a variável Authenticated
 	if true {
