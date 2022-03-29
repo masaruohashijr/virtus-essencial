e1d24ef6db7c4ec9a8d712122bf39ceeb7efa1c2
Masaru Ohashi Jr <masaru.junior@previc.gov.br>
2022-03-29 15:03:02 -0300

    1.3.92

--- src/virtus-essencial/main-essencial.go
+++ src/virtus-essencial/main-essencial.go
@@ -82,7 +82,7 @@ func main() {
 	sConfig := ReadConfig(SERVER)
 	sec.Store = sessions.NewCookieStore([]byte(sConfig.EncryptionKey))
 	hd.Db = dbConn()
-	mdl.Ambiente = " [" + sConfig.Ambiente + " 1.3.91" + "]"
+	mdl.Ambiente = " [" + sConfig.Ambiente + " 1.3.92" + "]"
 	mdl.AppName += mdl.Ambiente
 	// injetando a variável Authenticated
 	if false {
